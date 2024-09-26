1e6e9f63f9ecf93350af857764771d0d832717ac
Bob Park <hwpark@malgn.com>
2024-09-26 10:48:09 +0900

    feat: chat room user table ddl 추가

--- src/main/resources/db/migration/V0.000_2024_0925__add-table-chat.sql
+++ src/main/resources/db/migration/V0.000_2024_0925__add-table-chat.sql
@@ -3,9 +3,17 @@ create table chat_rooms
     id                 bigserial               not null primary key,
     name               varchar(100)            not null,
     description        text,
-    user_id            varchar(100)            not null,
     created_date       timestamp default now() not null,
     last_modified_date timestamp
+);
+
+create table chat_rooms_users
+(
+    id      bigserial    not null primary key,
+    room_id bigint       not null,
+    user_id varchar(100) not null,
+
+    foreign key (room_id) references chat_rooms (id)
 );
 
 create table chats
